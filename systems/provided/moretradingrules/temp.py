fc61f0174960231450bd009936839a003c51ca89
Robert Carver <rob@qoppac.com>
2016-06-11 19:32:43 +0100

    updated test of issue

--- systems/provided/moretradingrules/temp.py
+++ systems/provided/moretradingrules/temp.py
@@ -3,8 +3,9 @@ from sysdata.configdata import Config
 my_config=Config("examples.breakout.breakoutfuturesestimateconfig.yaml")
 
 system=futures_system(config=my_config)
-system.config.notional_trading_capital=10000000
+system.config.notional_trading_capital=100000
 system.set_logging_level("on")
+system.accounts.portfolio().stats()
 system.portfolio.get_instrument_weights().plot()
 from matplotlib.pyplot import show
 show()
