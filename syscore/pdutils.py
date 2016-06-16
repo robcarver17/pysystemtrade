35c51a895d709802eb62f8208af909fb7926fbcb
Robert Carver <rob@qoppac.com>
2016-06-16 06:15:37 +0100

    fixed drawdown overflow

--- syscore/pdutils.py
+++ syscore/pdutils.py
@@ -219,8 +219,9 @@ def drawdown(x):
     :param x: pd.DataFrame or Series
 
     :returns: pd.DataFrame or Series
+    
     """
-    maxx = pd.rolling_max(x, 99999999, min_periods=1)
+    maxx = pd.rolling_max(x, len(x), min_periods=1)
     return x - maxx
 
 
