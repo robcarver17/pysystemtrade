21fba73bf04471b8035520e0e7049636795d2ccb
Andy Geach <andy@bugorfeature.net>
2020-06-07 07:38:58 +0100

    fix version to match README

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ print(package_data)
 
 setup(
     name="pysystemtrade",
-    version="0.0.1",
+    version="0.30.0",
     author="Robert Carver",
     description=
     ("Python framework for running systems as in Robert Carver's book Systematic Trading"
