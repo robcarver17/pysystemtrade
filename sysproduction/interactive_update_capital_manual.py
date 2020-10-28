6412507d658bc4de17878b5332c41757ced9470b
robcarver17 <rob@systematicmoney.org>
2020-10-28 15:59:20 +0000

    refactor interactive capital

--- sysproduction/interactive_update_capital_manual.py
+++ sysproduction/interactive_update_capital_manual.py
@@ -1,12 +1,11 @@
-from sysbrokers.IB.ib_connection import connectionIB
 
 from syscore.objects import success, failure, missing_data, arg_not_supplied
 from syscore.dateutils import get_datetime_input
-from syscore.genutils import get_and_convert
+from syscore.genutils import get_and_convert, print_menu_and_get_response
 
 from sysproduction.data.get_data import dataBlob
 from sysproduction.data.capital import dataCapital
-
+from sysproduction.data.broker import dataBroker
 
 def interactive_update_capital_manual():
     """
@@ -22,19 +21,19 @@ def interactive_update_capital_manual():
         while still_running:
             # display capital and get input
             user_option_int = print_capital_and_get_user_input(data_capital)
-            if user_option_int == 0:
-                setup_initial_capital(data_capital)
-            elif user_option_int == 1:
-                update_capital_from_ib(data_capital)
+            if user_option_int == 1:
+                setup_initial_capital(data)
             elif user_option_int == 2:
-                adjust_capital_for_delta(data_capital)
+                update_capital_from_ib(data)
             elif user_option_int == 3:
-                modify_any_value(data_capital)
+                adjust_capital_for_delta(data_capital)
             elif user_option_int == 4:
+                modify_any_value(data_capital)
+            elif user_option_int == 5:
                 delete_capital_since_time(data_capital)
             elif user_option_int == 919:
                 delete_all_capital(data_capital)
-            elif user_option_int == 5:
+            elif user_option_int == 0:
                 still_running = False
                 break
             else:
@@ -47,66 +46,49 @@ def interactive_update_capital_manual():
     return success
 
 
-def print_capital_and_get_user_input(capital_data):
-    invalid_input = True
-    while invalid_input:
-        all_calcs = capital_data.total_capital_calculator.get_all_capital_calcs()
-        print("\n")
-        if all_calcs is missing_data:
-            # No capital
-            no_capital_setup = True
-            print("No capital setup yet")
-        else:
-            no_capital_setup = False
-            print(all_calcs.tail(10))
+def print_capital_and_get_user_input(capital_data: dataCapital):
+    all_calcs = capital_data.total_capital_calculator.get_all_capital_calcs()
+    print("\n")
+    if all_calcs is missing_data:
+        # No capital
+        no_capital_setup = True
+        print("No capital setup yet")
+    else:
+        no_capital_setup = False
+        print(all_calcs.tail(10))
 
-        print("\n")
-        possible_options = [
-            (0, "0: Setup initial capital parameters"),
-            (1, "1: Update capital from IB account value"),
-            (2, "2: Adjust account value for withdrawal or deposit"),
-            (3, "3: Modify any/all values"),
-            (4, "4: Delete values of capital since time T"),
-            (919, "919: Delete everything and start again"),
-            (5, "5: Exit"),
-        ]
+    print("\n")
 
-        if no_capital_setup:
-            possible_options = possible_options[:1] + possible_options[-1:]
-        else:
-            possible_options = possible_options[1:]
 
-        option_keys = [x[0] for x in possible_options]
-        option_strings = [x[1] for x in possible_options]
+    if no_capital_setup:
+        possible_options = {
+            1: "Setup initial capital parameters"}
+    else:
+        possible_options = {
+            2: "Update capital from IB account value",
+            3: "2: Adjust account value for withdrawal or deposit",
+            4: "3: Modify any/all values",
+            5: "4: Delete values of capital since time T",
+            919: "Delete everything and start again"}
 
-        print("\n".join(option_strings))
-        user_option = input("\nWhat would you like to do?")
-        try:
-            user_option_int = int(user_option)
-            assert user_option_int in option_keys
-            invalid_input = False
-            break
-        except BaseException:
-            input(
-                "\n%s is not a valid option: press return to continue"
-                % str(user_option)
-            )
-            continue
+    user_option_int = print_menu_and_get_response(possible_options, default_option=0,
+                                                  default_str="EXIT")
 
     return user_option_int
 
 
-def setup_initial_capital(data_capital):
+def setup_initial_capital(data: dataBlob):
     (
         broker_account_value,
         total_capital,
         maximum_capital,
         acc_pandl,
-    ) = get_initial_capital_values_from_user(data_capital)
+    ) = get_initial_capital_values_from_user(data)
     ans = input(
         "Are you sure about this? Will delete all existing capital (not for individual strategies) Yes/<anything else>"
     )
     if ans == "Yes":
+        data_capital = dataCapital(data)
         data_capital.total_capital_calculator.create_initial_capital(
             broker_account_value,
             total_capital=arg_not_supplied,
@@ -118,7 +100,7 @@ def setup_initial_capital(data_capital):
     return success
 
 
-def get_initial_capital_values_from_user(data_capital):
+def get_initial_capital_values_from_user(data: dataBlob):
     broker_account_value = get_and_convert(
         "Broker account value",
         type_expected=float,
@@ -126,7 +108,7 @@ def get_initial_capital_values_from_user(data_capital):
         default_value=arg_not_supplied,
     )
     if broker_account_value is arg_not_supplied:
-        broker_account_value = data_capital.get_ib_total_capital_value()
+        broker_account_value = get_broker_account_value(data)
         print("Got broker account value of %f from IB" % broker_account_value)
     total_capital = get_and_convert(
         "Total capital at risk",
@@ -144,8 +126,10 @@ def get_initial_capital_values_from_user(data_capital):
     return broker_account_value, total_capital, maximum_capital, acc_pandl
 
 
-def update_capital_from_ib(data_capital):
-    broker_account_value = data_capital.get_ib_total_capital_value()
+def update_capital_from_ib(data: dataBlob):
+    data_capital = dataCapital(data)
+    broker_account_value = get_broker_account_value(data)
+
     try:
         total_capital = data_capital.total_capital_calculator.get_total_capital_with_new_broker_account_value(
             broker_account_value, check_limit=0.1)
@@ -161,8 +145,13 @@ def update_capital_from_ib(data_capital):
 
     print("New total capital is %s" % total_capital)
 
+def get_broker_account_value(data: dataBlob):
+    data_broker = dataBroker(data)
+    capital_value = data_broker.get_total_capital_value_in_base_currency()
 
-def adjust_capital_for_delta(data_capital):
+    return capital_value
+
+def adjust_capital_for_delta(data_capital: dataCapital):
     capital_delta = get_and_convert(
         "What change have you made to brokerage account that will not change capital +ve deposit, -ve withdrawal",
         type_expected=float,
@@ -182,7 +171,7 @@ def adjust_capital_for_delta(data_capital):
     return success
 
 
-def modify_any_value(data_capital):
+def modify_any_value(data_capital: dataCapital):
     (
         broker_account_value,
         total_capital,
@@ -234,7 +223,7 @@ def get_values_from_user_to_modify():
     return broker_account_value, total_capital, maximum_capital, acc_pandl
 
 
-def delete_capital_since_time(data_capital):
+def delete_capital_since_time(data_capital:dataCapital):
     start_date = get_datetime_input("Delete capital from when?")
     ans = input("Are you sure about this? Can't be undone Yes/<other for no>")
     if ans == "Yes":
@@ -247,7 +236,7 @@ def delete_capital_since_time(data_capital):
     return success
 
 
-def delete_all_capital(data_capital):
+def delete_all_capital(data_capital: dataCapital):
     ans = input(
         "Will delete all capital history (though not for individual strategies). Really sure this is a good idea? Can't be recovered from: Yes/<anything else for no>"
     )
