19f7393aff842010e4350a1a8de52541dc9e992a
robcarver17 <rob@systematicmoney.org>
2020-10-30 14:47:56 +0000

    expiry date updater needed

--- sysproduction/update_sampled_contracts.py
+++ sysproduction/update_sampled_contracts.py
@@ -296,7 +296,7 @@ def update_expiry_for_contract(contract_object, data):
         return None
 
     # Different!
-    contract_object.contract_date.expiry_date = ib_expiry_date.as_tuple()
+    contract_object.contract_date.update_expiry_date(ib_expiry_date)
     update_contracts.add_contract_data(
         contract_object, ignore_duplication=True)
 
