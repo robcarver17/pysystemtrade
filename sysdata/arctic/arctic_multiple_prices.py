89424d2ca9638eb835fdfae9b991ae9e0cd443ca
robcarver17 <rob@systematicmoney.org>
2020-11-23 12:05:13 +0000

    refactored multiple prices arctic

--- sysdata/arctic/arctic_multiple_prices.py
+++ sysdata/arctic/arctic_multiple_prices.py
@@ -9,7 +9,7 @@ from sysdata.futures.multiple_prices import (
 )
 from sysobjects.multiple_prices import futuresMultiplePrices
 from sysobjects.dict_of_named_futures_per_contract_prices import list_of_price_column_names, \
-    list_of_contract_column_names
+     contract_name_from_column_name
 from syslogdiag.log import logtoscreen
 
 MULTIPLE_COLLECTION = "futures_multiple_prices"
@@ -28,54 +28,56 @@ class arcticFuturesMultiplePricesData(futuresMultiplePricesData):
 
         self._arctic = articConnection(MULTIPLE_COLLECTION, mongo_db=mongo_db)
 
-        self.name = (
-            "simData connection for multiple futures prices, arctic %s/%s @ %s " %
+
+    def __repr__(self):
+        return             "simData connection for multiple futures prices, arctic %s/%s @ %s " % \
             (self._arctic.database_name,
              self._arctic.collection_name,
              self._arctic.host,
-             ))
+             )
 
-    def __repr__(self):
-        return self.name
+    @property
+    def arctic(self):
+        return self._arctic
 
-    def get_list_of_instruments(self):
-        return self._arctic.library.list_symbols()
+    def get_list_of_instruments(self) -> list:
+        return self.arctic.get_keynames()
 
-    def _get_multiple_prices_without_checking(self, instrument_code):
-        item = self._arctic.library.read(instrument_code)
-
-        # Returns a data frame which should have the right format
-        data = item.data
+    def _get_multiple_prices_without_checking(self, instrument_code: str) ->futuresMultiplePrices:
+        data =self.arctic.read(instrument_code)
 
         return futuresMultiplePrices(data)
 
     def _delete_multiple_prices_without_any_warning_be_careful(
-            self, instrument_code):
-        self.log.label(instrument_code=instrument_code)
-        self._arctic.library.delete(instrument_code)
+            self, instrument_code: str):
+
+        self.arctic.delete(instrument_code)
         self.log.msg(
             "Deleted multiple prices for %s from %s" %
-            (instrument_code, self.name))
+            (instrument_code, str(self)), instrument_code)
 
     def _add_multiple_prices_without_checking_for_existing_entry(
-        self, instrument_code, multiple_price_data_object
+        self, instrument_code: str, multiple_price_data_object: futuresMultiplePrices
     ):
 
         multiple_price_data_aspd = pd.DataFrame(multiple_price_data_object)
+        multiple_price_data_aspd = _change_contracts_to_str(multiple_price_data_aspd)
 
-        for contract_column in list_of_contract_column_names:
-            multiple_price_data_aspd[contract_column] = multiple_price_data_aspd[
-                contract_column
-            ].astype(str)
-
-        for price_column in list_of_price_column_names:
-            multiple_price_data_aspd[price_column] = multiple_price_data_aspd[
-                price_column
-            ].astype(float)
-
-        self.log.label(instrument_code=instrument_code)
-        self._arctic.library.write(instrument_code, multiple_price_data_aspd)
+        self.arctic.write(instrument_code, multiple_price_data_aspd)
         self.log.msg(
             "Wrote %s lines of prices for %s to %s"
-            % (len(multiple_price_data_aspd), instrument_code, self.name)
+            % (len(multiple_price_data_aspd), instrument_code, str(self)), instrument_code = instrument_code
         )
+
+def _change_contracts_to_str(multiple_price_data_aspd):
+    for price_column in list_of_price_column_names:
+        multiple_price_data_aspd[price_column] = multiple_price_data_aspd[
+            price_column
+        ].astype(float)
+
+        contract_column  = contract_name_from_column_name(price_column)
+        multiple_price_data_aspd[contract_column] = multiple_price_data_aspd[
+            contract_column
+        ].astype(str)
+
+    return multiple_price_data_aspd
