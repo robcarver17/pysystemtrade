3833f960de971bac370f1efed864a24a5615ca46
robcarver17 <rob@systematicmoney.org>
2020-11-25 15:44:52 +0000

    avoid execution spaming

--- sysexecution/stack_handler/create_broker_orders_from_contract_orders.py
+++ sysexecution/stack_handler/create_broker_orders_from_contract_orders.py
@@ -171,12 +171,6 @@ class stackHandlerCreateBrokerOrders(stackHandlerCore):
                 str(liquid_qty), str(contract_order_after_trade_limits.trade)))
 
         if liquid_qty.equals_zero():
-            # Nothing we can do here
-            log.msg(
-                "Can't do any of size %s so not trading at all"
-                % str(contract_order_after_trade_limits.trade)
-            )
-
             return missing_order
 
         contract_order = contract_order_after_trade_limits.replace_trade_only_use_for_unsubmitted_trades(
