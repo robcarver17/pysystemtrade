e735a372645b95392212a27c4af060ff7b4d1123
robcarver17 <rob@systematicmoney.org>
2020-11-25 09:03:13 +0000

    added print statement to init from csv

--- sysinit/futures/contract_prices_from_csv_to_arctic.py
+++ sysinit/futures/contract_prices_from_csv_to_arctic.py
@@ -2,20 +2,22 @@ from sysdata.csv.csv_futures_contract_prices import csvFuturesContractPriceData
 from sysdata.arctic.arctic_futures_per_contract_prices import arcticFuturesContractPriceData
 from sysobjects.contracts import futuresContract
 
-DEFAULT_PATH = "/home/rob/data/backup/contract_prices/"
 
-def init_arctic_with_csv_futures_contract_prices(datapath):
+def init_arctic_with_csv_futures_contract_prices(datapath: str):
     csv_prices = csvFuturesContractPriceData(datapath)
-    input("WARNING THIS WILL ERASE ANY EXISTING ARCTIC PRICES WITH DATA FROM %s ARE YOU SURE?!" % csv_prices.datapath)
+    input("WARNING THIS WILL ERASE ANY EXISTING ARCTIC PRICES WITH DATA FROM %s ARE YOU SURE?! (CTRL-C TO STOP)" % csv_prices.datapath)
 
     instrument_codes = csv_prices.get_list_of_instrument_codes_with_price_data()
+    instrument_codes.sort()
     for instrument_code in instrument_codes:
         init_arctic_with_csv_futures_contract_prices_for_code(instrument_code, datapath)
 
-def init_arctic_with_csv_futures_contract_prices_for_code(instrument_code:str, datapath: str = DEFAULT_PATH):
+def init_arctic_with_csv_futures_contract_prices_for_code(instrument_code:str, datapath: str):
     print(instrument_code)
     csv_prices = csvFuturesContractPriceData(datapath)
     arctic_prices = arcticFuturesContractPriceData()
+
+    print("Getting .csv prices may take some time")
     csv_price_dict = csv_prices.get_all_prices_for_instrument(instrument_code)
 
     for contract_date_str, prices_for_contract in csv_price_dict.items():
