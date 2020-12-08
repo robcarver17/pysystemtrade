e04c3b0cb1cefaa7dce58c0389abb44df4de172e
robcarver17 <rob@systematicmoney.org>
2020-12-08 17:37:56 +0000

    slightly nicer process

--- syscontrol/monitor.py
+++ syscontrol/monitor.py
@@ -46,24 +46,20 @@ class processObservatory(dict):
         ## get initial status
         self.update_all_status_with_process_control()
 
-    def html_repr(self):
-        list_of_str = self.list_of_processes_str()
-        all_str = "<br/>".join(list_of_str)
-        return all_str
 
-    def list_of_processes_str(self):
+    def lol_repr(self):
         list_of_processes =get_list_of_process_names(self)
-        list_of_str = [self.str_for_process(process_name)
+        list_of_str = [self.list_of_str_for_process(process_name)
                        for process_name in list_of_processes]
 
         return list_of_str
 
-    def str_for_process(self, process_name):
-        pname = f"{''+process_name:<30}"
+    def list_of_str_for_process(self, process_name):
+
         control_str = str(get_control_for_process(self, process_name))
         status_str = self.get_current_status(process_name)
 
-        return "%s: %s (%s)" %  (pname, control_str, status_str)
+        return [process_name, control_str, status_str]
 
     @property
     def data(self):
@@ -146,12 +142,19 @@ def generate_html(process_observatory: processObservatory):
     with open(resolved_filename, "w") as file:
         file.write("<br/> Last update %s" % str(datetime.datetime.now()))
         file.write("<br/><br/>")
-        file.write(process_observatory.html_repr())
+        html_table(file, process_observatory.lol_repr())
         file.write("<br/><br/>")
         file.write(process_observatory.log_messages.html_repr())
         file.write("<br/><br/>")
 
 
+def html_table(file, lol: list):
+  file.write('<table>')
+  for sublist in lol:
+    file.write('  <tr><td>')
+    file.write('    </td><td>'.join(sublist))
+    file.write('  </td></tr>')
+  file.write('</table>')
 
 if __name__ == "__main__":
     monitor()
