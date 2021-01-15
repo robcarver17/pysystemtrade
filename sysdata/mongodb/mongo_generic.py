d01187d62e957fda399e824dd74e7c9f6e39b75b
robcarver17 <rob@systematicmoney.org>
2021-01-15 09:55:23 +0000

    small bug in mongo data

--- sysdata/mongodb/mongo_generic.py
+++ sysdata/mongodb/mongo_generic.py
@@ -148,12 +148,12 @@ class mongoDataWithMultipleKeys(object):
     def __repr__(self):
         return self.name
 
-    @property
+    @propertys
     def name(self) -> str:
         mongo_object = self._mongo
         name = (
-            "mongoData connection for %s, mongodb %s/%s @ %s -p %s " %
-            (mongo_object.collection_name,
+            "mongoData connection for mongodb %s/%s @ %s -p %s " %
+            (
             mongo_object.database_name,
              mongo_object.collection_name,
              mongo_object.host,
