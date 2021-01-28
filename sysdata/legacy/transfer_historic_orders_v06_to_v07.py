4517dd1e6aa65f6292e548ca3c301c44459859b3
robcarver17 <rob@systematicmoney.org>
2021-01-28 13:50:49 +0000

    temp file change

--- sysdata/legacy/transfer_historic_orders_v06_to_v07.py
+++ sysdata/legacy/transfer_historic_orders_v06_to_v07.py
@@ -4,18 +4,18 @@ d = dataOrders()
 
 if __name__ == "__main__":
 
-list_of_order_ids = d.data.db_broker_historic_orders.get_list_of_order_ids()
-for order_id in list_of_order_ids:
-    order = d.data.db_broker_historic_orders.get_order_with_orderid(order_id)
-    d.data.db_broker_historic_orders._add_order_to_data_no_checking(order)
+    list_of_order_ids = d.data.db_broker_historic_orders.get_list_of_order_ids()
+    for order_id in list_of_order_ids:
+        order = d.data.db_broker_historic_orders.get_order_with_orderid(order_id)
+        d.data.db_broker_historic_orders._add_order_to_data_no_checking(order)
 
 
-list_of_order_ids = d.data.db_contract_historic_orders.get_list_of_order_ids()
-for order_id in list_of_order_ids:
-    order = d.data.db_contract_historic_orders.get_order_with_orderid(order_id)
-    d.data.db_contract_historic_orders._add_order_to_data_no_checking(order)
+    list_of_order_ids = d.data.db_contract_historic_orders.get_list_of_order_ids()
+    for order_id in list_of_order_ids:
+        order = d.data.db_contract_historic_orders.get_order_with_orderid(order_id)
+        d.data.db_contract_historic_orders._add_order_to_data_no_checking(order)
 
-list_of_order_ids = d.data.db_strategy_historic_orders.get_list_of_order_ids()
-for order_id in list_of_order_ids:
-    order = d.data.db_strategy_historic_orders.get_order_with_orderid(order_id)
-    d.data.db_strategy_historic_orders._add_order_to_data_no_checking(order)
+    list_of_order_ids = d.data.db_strategy_historic_orders.get_list_of_order_ids()
+    for order_id in list_of_order_ids:
+        order = d.data.db_strategy_historic_orders.get_order_with_orderid(order_id)
+        d.data.db_strategy_historic_orders._add_order_to_data_no_checking(order)
