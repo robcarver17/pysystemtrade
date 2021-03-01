51d622e14f16da33ee4638047fb1541ef741cecf
robcarver17 <rob@systematicmoney.org>
2021-03-01 15:15:03 +0000

    main update methods now ask for strat name

--- sysproduction/update_strategy_orders.py
+++ sysproduction/update_strategy_orders.py
@@ -1,9 +1,10 @@
-
+from syscore.genutils import print_menu_of_values_and_get_response
 from syscontrol.strategy_tools import strategyRunner
 
 from sysdata.data_blob import dataBlob
 from sysproduction.data.control_process import get_list_of_strategies_for_process
 from sysexecution.strategies.strategy_order_handling import name_of_main_generator_method
+from sysproduction.data.strategies import get_valid_strategy_name_from_user
 
 process_name = "run_strategy_order_generator"
 
@@ -11,7 +12,15 @@ process_name = "run_strategy_order_generator"
 def update_strategy_orders():
     ## function if called from script
     with dataBlob(log_name="Update-Strategy-Orders") as data:
+
         list_of_strategies = get_list_of_strategies_for_process(data, process_name)
+        ALL = "ALL"
+        print("Which strategy?")
+        strategy_name = print_menu_of_values_and_get_response(list_of_strategies, default_str=ALL)
+
+        if not strategy_name == ALL:
+            list_of_strategies = [strategy_name]
+
         for strategy_name in list_of_strategies:
             strategy_order_generator = strategyRunner(data, strategy_name, process_name, name_of_main_generator_method)
             strategy_order_generator.run_strategy_method()
--- sysproduction/update_system_backtests.py
+++ sysproduction/update_system_backtests.py
@@ -1,8 +1,9 @@
 from syscontrol.strategy_tools import strategyRunner
-
+from syscore.genutils import print_menu_of_values_and_get_response
 
 from sysdata.data_blob import dataBlob
 from sysproduction.data.control_process import get_list_of_strategies_for_process
+
 
 backtest_function = "run_backtest"
 process_name = "run_systems"
@@ -12,6 +13,14 @@ def update_system_backtests():
     ## function if called from script
     with dataBlob(log_name="Update-System_Backtest") as data:
         list_of_strategies = get_list_of_strategies_for_process(data, process_name)
+        ALL = "ALL"
+        print("Which strategy?")
+        strategy_name = print_menu_of_values_and_get_response(list_of_strategies, default_str=ALL)
+
+        if not strategy_name == ALL:
+            list_of_strategies = [strategy_name]
+
+
         for strategy_name in list_of_strategies:
             system_backtest_runner = strategyRunner(data, strategy_name, process_name, backtest_function)
             system_backtest_runner.run_strategy_method()
