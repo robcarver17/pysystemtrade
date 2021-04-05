fb0643b2239a79f59a56602da72cb87e0b37bada
Andy Geach <andy@bugorfeature.net>
2021-04-05 18:07:52 +0100

    more mongo url scenarios added

--- sysdata/mongodb/tests/test_mongodb.py
+++ sysdata/mongodb/tests/test_mongodb.py
@@ -5,9 +5,27 @@ class TestMongoDB:
 
     def test_hide_password(self):
 
-        # examples from https://docs.mongodb.com/manual/reference/connection-string/
+        # url examples from https://docs.mongodb.com/manual/reference/connection-string/
 
-        #host_pattern = re.compile('^(mongodb://)([^:]+):([^@]+)@([^/]+)')
+        ip = "mongodb://127.0.0.1/production"
+        clean_ip = clean_mongo_host(ip)
+        print(clean_ip)
+        assert clean_ip == ip
+
+        simple = "mongodb://localhost/production"
+        clean_simple = clean_mongo_host(simple)
+        print(clean_simple)
+        assert clean_simple == simple
+
+        simple_pw = "mongodb://myDBReader:D1fficultP%40ssw0rd@localhost/production"
+        clean_simple_pw = clean_mongo_host(simple_pw)
+        print(clean_simple_pw)
+        assert 'D1fficultP%40ssw0rd' not in clean_simple_pw
+
+        simple_port = "mongodb://localhost:28018/production"
+        clean_simple_port = clean_mongo_host(simple_port)
+        print(simple_port)
+        assert clean_simple_port == simple_port
 
         standalone = "mongodb://myDBReader:D1fficultP%40ssw0rd@mongodb0.example.com:27017/?authSource=admin"
         clean_standalone = clean_mongo_host(standalone)
