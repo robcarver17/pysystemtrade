8249dd92e9789d9fbb54a346525e244240855680
rob <robcarver17@googlemail.com>
2021-04-23 13:56:08 +0100

    subsystem pandl now has cash costs too

--- systems/accounts/pandl_calculators/pandl_cash_costs.py
+++ systems/accounts/pandl_calculators/pandl_cash_costs.py
@@ -3,16 +3,16 @@ import pandas as pd
 from syscore.objects import arg_not_supplied
 from systems.accounts.pandl_calculators.pandl_calculation import pandlCalculationWithGenericCosts
 
+from sysobjects.instruments import instrumentCosts
 
-class pandlCalculationWithActualCosts(pandlCalculationWithGenericCosts):
+
+class pandlCalculationWithCashCosts(pandlCalculationWithGenericCosts):
     def __init__(self, *args,
-                 SR_cost: float,
-                 daily_returns_volatility: pd.Series = arg_not_supplied,
+                 raw_costs: instrumentCosts,
                  **kwargs):
         ## Is SR_cost a negative number?
         super().__init__(*args, **kwargs)
-        self._SR_cost = SR_cost
-        self._daily_returns_volatility = daily_returns_volatility
+        self._raw_costs = raw_costs
 
     def costs_pandl_in_points(self) -> pd.Series:
         SR_cost_as_annualised_figure = self.SR_cost_as_annualised_figure_points()
@@ -23,7 +23,9 @@ class pandlCalculationWithActualCosts(pandlCalculationWithGenericCosts):
         SR_cost_per_period = calculate_SR_cost_per_period_of_position_data_match_price_index(position,
                                                                                              price=price,
                                                                            SR_cost_as_annualised_figure=SR_cost_as_annualised_figure)
-
+    @property
+    def raw_costs(self) -> instrumentCosts:
+        return self._raw_costs
 
 
         return SR_cost_per_period
