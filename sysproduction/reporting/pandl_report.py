975cdc7e4aacaac3ae3c2d2228ff3d2fe5f5fdb9
rob <robcarver17@googlemail.com>
2021-04-29 17:15:33 +0100

    no need to multiply p&l by 100 in report

--- sysproduction/reporting/pandl_report.py
+++ sysproduction/reporting/pandl_report.py
@@ -83,7 +83,7 @@ def get_pandl_report_data(data, start_date, end_date):
     data.temp_pandl_write = types.MethodType(temp_pandl_write, data)
 
     total_capital_pandl = (
-        get_total_capital_pandl(data, start_date, end_date=end_date) * 100
+        get_total_capital_pandl(data, start_date, end_date=end_date)
     )
     pandl_for_instruments_across_strategies = (
         get_ranked_list_of_pandl_by_instrument_all_strategies_in_date_range(
@@ -178,7 +178,7 @@ def get_strategy_pandl_and_residual(data, start_date, end_date):
     residual_dfrow = pd.DataFrame(
         dict(codes=["residual"], pandl=residual_pandl))
     strategies_pandl = strategies_pandl.append(residual_dfrow)
-    strategies_pandl.pandl = strategies_pandl.pandl * 100
+    strategies_pandl.pandl = strategies_pandl.pandl
 
     return strategies_pandl
 
@@ -298,7 +298,7 @@ def get_period_perc_pandl_for_sector_in_date_range(
     ]
     asset_class_pandl = sum(instrument_pandl)
 
-    return asset_class_pandl * 100
+    return asset_class_pandl 
 
 
 def get_period_perc_pandl_for_instrument_all_strategies_in_date_range(
