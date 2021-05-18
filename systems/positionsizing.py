1e808f09f20d34bbaa074c65a692be2cf20fcdc0
rob <robcarver17@googlemail.com>
2021-05-18 17:15:50 +0100

    fixed reindexing bug in position sizing code

--- systems/positionsizing.py
+++ systems/positionsizing.py
@@ -178,9 +178,9 @@ class PositionSizing(SystemStage):
         instr_ccy_vol = self.get_instrument_currency_vol(instrument_code)
         fx_rate = self.get_fx_rate(instrument_code)
 
-        fx_rate = fx_rate.reindex(instr_ccy_vol.index)
+        fx_rate = fx_rate.reindex(instr_ccy_vol.index, method="ffill")
 
-        instr_value_vol = instr_ccy_vol.ffill() * fx_rate.ffill()
+        instr_value_vol = instr_ccy_vol.ffill() * fx_rate
 
         return instr_value_vol
 
