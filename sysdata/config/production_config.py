18d4ff80d08cd2626d2f2cc521eab1fdb9b1ee0a
bollard <ryanwballard@gmail.com>
2021-06-17 00:14:00 -0500

    Include file name to make warning a little friendlier

--- sysdata/config/production_config.py
+++ sysdata/config/production_config.py
@@ -8,7 +8,7 @@ def get_production_config() -> Config:
     if private_config_file_exists():
         config = Config(PRIVATE_CONFIG_FILE)
     else:
-        print("Private configuration %s does not exist; no problem if running in sim mode")
+        print("Private configuration %s does not exist; no problem if running in sim mode" % PRIVATE_CONFIG_FILE)
         config = Config({})
 
     config.fill_with_defaults()
