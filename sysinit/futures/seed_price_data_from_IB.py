89a9032bb705be19a1b716ace36266564fb63846
rob <robcarver17@googlemail.com>
2021-08-26 14:54:21 +0100

    roll calendars

--- sysinit/futures/seed_price_data_from_IB.py
+++ sysinit/futures/seed_price_data_from_IB.py
@@ -1,10 +1,12 @@
 from sysdata.data_blob import dataBlob
 from sysbrokers.IB.ib_futures_contract_price_data import ibFuturesContractPriceData, futuresContract
+from sysbrokers.IB.ib_futures_contracts_data import ibFuturesContractData
 from sysdata.arctic.arctic_futures_per_contract_prices import arcticFuturesContractPriceData
 
 def seed_price_data_from_IB(instrument_code):
     data = dataBlob()
-    data.add_class_list([ibFuturesContractPriceData, arcticFuturesContractPriceData])
+    data.add_class_list([ibFuturesContractPriceData, arcticFuturesContractPriceData,
+                         ibFuturesContractData])
     list_of_contracts = data.\
         broker_futures_contract_price.\
         contracts_with_price_data_for_instrument_code(instrument_code,allow_expired=True)
@@ -13,7 +15,10 @@ def seed_price_data_from_IB(instrument_code):
         seed_price_data_for_contract(data, contract)
 
 def seed_price_data_for_contract(data: dataBlob, contract: futuresContract):
-    print("ASSUMING ROLL YYYYMM IS SAME AS EXPIRY: WILL BE DIFFERENT FOR CERTAIN ENERGY CONTRACTS")
+    ## We do this slightly tortorous thing because there are energy contracts
+    ## which don't expire in the month they are labelled with
+    ## So for example, CRUDE_W 202106 actually expires on 20210528
+
     date_str = contract.contract_date.date_str[:6]
     new_contract = futuresContract(contract.instrument,
                                    date_str)
@@ -23,7 +28,8 @@ def seed_price_data_for_contract(data: dataBlob, contract: futuresContract):
     if len(prices)==0:
         print("No data!")
 
-    ## Use update to avoid issues with overwriting
+    ## If you want to modify this script so it updates existing prices
+    ## eg from barchart, then uncomment the following line and comment the next
     #data.db_futures_contract_price.update_prices_for_contract(contract, prices)
     data.db_futures_contract_price.write_prices_for_contract_object(new_contract, prices, ignore_duplication=True)
 
