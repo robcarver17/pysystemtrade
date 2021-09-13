9f4915085b4925f95a9e83c3a31151354f8d75d7
rob <robcarver17@googlemail.com>
2021-09-13 09:40:46 +0100

    seed IB price data no longer writes empty data frames

--- sysinit/futures/seed_price_data_from_IB.py
+++ sysinit/futures/seed_price_data_from_IB.py
@@ -27,11 +27,11 @@ def seed_price_data_for_contract(data: dataBlob, contract: futuresContract):
         get_prices_at_frequency_for_potentially_expired_contract_object(new_contract)
     if len(prices)==0:
         print("No data!")
-
-    ## If you want to modify this script so it updates existing prices
-    ## eg from barchart, then uncomment the following line and comment the next
-    #data.db_futures_contract_price.update_prices_for_contract(contract, prices)
-    data.db_futures_contract_price.write_prices_for_contract_object(new_contract, prices, ignore_duplication=True)
+    else:
+        ## If you want to modify this script so it updates existing prices
+        ## eg from barchart, then uncomment the following line and comment the next
+        #data.db_futures_contract_price.update_prices_for_contract(contract, prices)
+        data.db_futures_contract_price.write_prices_for_contract_object(new_contract, prices, ignore_duplication=True)
 
 if __name__ == "__main__":
     print("Get initial price data from IB")
