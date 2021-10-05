21a1cd82e27d7b1e95aceff72cc3ee427fae94ee
James Ward <james@robomo.co>
2021-10-05 22:33:06 +0800

    Get capital

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,16 +4,25 @@ from sysdata.data_blob import dataBlob
 
 from sysproduction.data.prices import diagPrices
 from sysproduction.reporting import roll_report
+from sysproduction.data.capital import dataCapital
 
 from pprint import pprint
 
 app = Flask(__name__)
 
+data = dataBlob(log_name="dashboard")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/capital")
+def capital():
+    capital_data = dataCapital(data)
+    capital_series = capital_data.get_series_of_all_global_capital()
+    now = capital_series.iloc[-1]['Actual']
+    yesterday = capital_series.last('1D').iloc[0]['Actual']
+    return {'now': now, 'yesterday': yesterday}
 
 @app.route("/traffic_lights")
 def traffic_lights():
@@ -30,7 +39,6 @@ def traffic_lights():
 @app.route("/rolls")
 def rolls():
     # If we have a dictionary, Flask will automatically json-ify it
-    data = dataBlob(log_name="dashboard")
     diag_prices = diagPrices(data)
 
     all_instruments = diag_prices.get_list_of_instruments_in_multiple_prices()
--- dashboard/static/js/update.js
+++ dashboard/static/js/update.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
   $.ajax({
     type: "GET",
-    url: "/traffic_lights",
+    url: "/capital",
     success: function(data) {
-      $.each(data, function(k,v) {
-        //$("#"+k).addClass(v);
+      $('#capital-tl').html('$'+data['now'].toLocaleString());
+      if (data['now'] >= data['yesterday']) {
+        $('#capital-tl').addClass('green');
+      } else {
+        $('#capital-tl').addClass('green');
       }
-      );
     }
   }
   );
