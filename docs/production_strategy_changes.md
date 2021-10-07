2a42a75965f374177e8cee1cafc988be7cb39fbd
rob <robcarver17@googlemail.com>
2021-10-07 14:47:04 +0100

    finished strategy report for dynamic strategy

---
+++ docs/production_strategy_changes.md
@@ -0,0 +1,29 @@
+This document describes how you'd make changes to strategies in production: adding new strategies, or replacing existing ones.
+
+# Steps to follow
+
+It's important that the following steps are followed, in order.
+
+1. Ensure you have a working backtest, with a production configuration .yaml file
+2. Create a run_system to run the strategy
+3. Make a backup of your data
+4. Stop any processes running
+5. Update private_config.yaml and private_control_config.yaml
+6. Check the production backtest will run
+
+Not all the steps are described in detail in this document, see [the production documents](/docs/production.md) for details.
+
+# Create a run_system to run the strategy
+
+If you are using a completely vanilla 'out of the box' strategy that can be run with the default provided stages then this isn't neccessary. For clarity these are the default strategies:
+
+- [Classic system](/sysproduction/strategy_code/run_system_classic.py)
+- [Dynamic system](/sysproduction/strategy_code/run_dynamic_optimised_system.py) (as described [here](https://qoppac.blogspot.com/2021/10/mr-greedy-and-tale-of-minimum-tracking.html))
+
+Otherwise you will need to create your own runSystem class. There is an example [here](/examples/production/example_of_custom_run_system.py). Notice that it overrides `system_method` to create a different system from the default; that in turn is built up from some custom stages.
+
+You may also need to override the `run_backtest` method if you need your strategy to do something different in terms of saving optimised positions (again that is the principal difference between the classic and dynamic systems).
+
+# Update configuration files
+
+Even if you're replacing a strategy I strongly advise that you initially *add* your new strategies to configuration files before .
---
+++ examples/production/example_of_custom_run_system.py
@@ -0,0 +1,100 @@
+
+from syscore.objects import arg_not_supplied
+
+from sysdata.data_blob import dataBlob
+
+from sysproduction.data.sim_data import get_sim_data_object_for_production
+from sysproduction.strategy_code.run_dynamic_optimised_system import runSystemCarryTrendDynamic
+
+from syslogdiag.log_to_screen import logtoscreen
+
+from systems.basesystem import System
+
+
+class runMySystemCarryTrendDynamic(runSystemCarryTrendDynamic):
+
+    # DO NOT CHANGE THE NAME OF THIS FUNCTION; IT IS HARDCODED INTO CONFIGURATION FILES
+    # BECAUSE IT IS ALSO USED TO LOAD BACKTESTS
+    def system_method(self,
+                      notional_trading_capital: float=None,
+                      base_currency: str=None) -> System:
+        data = self.data
+        backtest_config_filename = self.backtest_config_filename
+
+        system = production_carry_trend_dynamic_system(
+            data,
+            backtest_config_filename,
+            log=data.log,
+            notional_trading_capital=notional_trading_capital,
+            base_currency=base_currency,
+        )
+
+        return system
+
+
+def production_carry_trend_dynamic_system(
+    data: dataBlob,
+    config_filename: str,
+    log=logtoscreen("futures_system"),
+    notional_trading_capital: float=arg_not_supplied,
+    base_currency: str=arg_not_supplied,
+) -> System:
+
+    log_level = "on"
+
+    sim_data = get_sim_data_object_for_production(data)
+    config = Config(config_filename)
+
+    # Overwrite capital and base currency
+    if notional_trading_capital is not arg_not_supplied:
+        config.notional_trading_capital = notional_trading_capital
+
+    if base_currency is not arg_not_supplied:
+        config.base_currency = base_currency
+
+    system = futures_system(data=sim_data, config=config)
+    system._log = log
+
+    system.set_logging_level(log_level)
+
+    return system
+
+from sysdata.config.configdata import Config
+
+from systems.forecasting import Rules
+from systems.basesystem import System
+from systems.forecast_combine import ForecastCombine
+from private.systems.carrytrend.forecastScaleCap import volAttenForecastScaleCap
+from private.systems.carrytrend.rawdata import myFuturesRawData
+from systems.positionsizing import PositionSizing
+from systems.portfolio import Portfolios
+from systems.provided.dynamic_small_system_optimise.portfolio_weights import portfolioWeightsStage
+from systems.provided.dynamic_small_system_optimise.optimised_positions import optimisedPositions
+from systems.provided.dynamic_small_system_optimise.risk import Risk
+from systems.provided.dynamic_small_system_optimise.accounts import accountForOptimisedStage
+
+def futures_system(data, config):
+
+    system = System(
+        [
+            Risk(),
+            accountForOptimisedStage(),
+            optimisedPositions(),
+            portfolioWeightsStage(),
+            Portfolios(),
+            PositionSizing(),
+            myFuturesRawData(),
+            ForecastCombine(),
+            volAttenForecastScaleCap(),
+            Rules(),
+
+        ],
+        data,
+        config,
+    )
+    system.set_logging_level("on")
+
+    return system
+
+
+
