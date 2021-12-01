de54ab391a770335d1eab3c1a3283e7887dc163e
rob <robcarver17@googlemail.com>
2021-12-01 11:30:41 +0000

    started moving my system yaml to public

---
+++ systems/rob_system/forecastScaleCap.py
@@ -0,0 +1,71 @@
+from systems.forecast_scale_cap import *
+from statsmodels.distributions.empirical_distribution import ECDF
+
+class volAttenForecastScaleCap(ForecastScaleCap):
+
+    @diagnostic()
+    def get_vol_quantile_points(self, instrument_code):
+        ## More properly this would go in raw data perhaps
+        self.log.msg("Calculating vol quantile for %s" % instrument_code)
+        daily_vol = self.parent.rawdata.get_daily_percentage_volatility(instrument_code)
+        ten_year_vol = daily_vol.rolling(2500, min_periods=10).mean()
+        normalised_vol = daily_vol / ten_year_vol
+
+        normalised_vol_q = quantile_of_points_in_data_series(normalised_vol)
+
+        return normalised_vol_q
+
+    @diagnostic()
+    def get_vol_attenuation(self, instrument_code):
+        normalised_vol_q = self.get_vol_quantile_points(instrument_code)
+        vol_attenuation = normalised_vol_q.apply(multiplier_function)
+
+        smoothed_vol_attenuation = vol_attenuation.ewm(span=10).mean()
+
+        return smoothed_vol_attenuation
+
+    @input
+    def get_raw_forecast_before_attenuation(self, instrument_code, rule_variation_name):
+        ## original code for get_raw_forecast
+        raw_forecast = self.parent.rules.get_raw_forecast(
+            instrument_code, rule_variation_name
+        )
+
+        return raw_forecast
+
+    @diagnostic()
+    def get_raw_forecast(self, instrument_code, rule_variation_name):
+        ## overriden methon this will be called downstream so don't change name
+        raw_forecast_before_atten = self.get_raw_forecast_before_attenuation(instrument_code, rule_variation_name)
+        use_attenuation = self.config.use_attenuation
+        if rule_variation_name not in use_attenuation:
+            return raw_forecast_before_atten
+        else:
+            vol_attenutation = self.get_vol_attenuation(instrument_code)
+
+            attenuated_forecast = raw_forecast_before_atten * vol_attenutation
+
+            return attenuated_forecast
+
+def quantile_of_points_in_data_series(data_series):
+    results = [quantile_of_points_in_data_series_row(data_series, irow) for irow in range(len(data_series))]
+    results_series = pd.Series(results, index = data_series.index)
+
+    return results_series
+
+
+# this is a little slow so suggestions for speeding up are welcome
+def quantile_of_points_in_data_series_row(data_series, irow):
+    if irow<2:
+        return np.nan
+    historical_data = list(data_series[:irow].values)
+    current_value = data_series[irow]
+    ecdf_s = ECDF(historical_data)
+
+    return ecdf_s(current_value)
+
+def multiplier_function(vol_quantile):
+    if np.isnan(vol_quantile):
+        return 1.0
+
+    return 2 - 1.5*vol_quantile
--- systems/rob_system/run_system.py
+++ systems/rob_system/run_system.py
@@ -1,48 +1,13 @@
 
 from syscore.objects import arg_not_supplied
-
-from sysdata.data_blob import dataBlob
-
-from sysproduction.data.sim_data import
-from sysproduction.strategy_code.run_dynamic_optimised_system import runSystemCarryTrendDynamic
-
-from syslogdiag.log_to_screen import logtoscreen
-
-from systems.basesystem import System
-
-
-def production_carry_trend_dynamic_system(
-    data: dataBlob,
-    config_filename: str,
-    log=logtoscreen("futures_system"),
-    notional_trading_capital: float=arg_not_supplied,
-    base_currency: str=arg_not_supplied,
-) -> System:
-
-    log_level = "on"
-    data = dataBlob()
-
-    # Overwrite capital and base currency
-    if notional_trading_capital is not arg_not_supplied:
-        config.notional_trading_capital = notional_trading_capital
-
-    if base_currency is not arg_not_supplied:
-        config.base_currency = base_currency
-
-    system = futures_system(data=sim_data, config=config)
-    system._log = log
-
-    system.set_logging_level(log_level)
-
-    return system
-
+from sysdata.sim.csv_futures_sim_data import csvFuturesSimData
 from sysdata.config.configdata import Config
 
 from systems.forecasting import Rules
 from systems.basesystem import System
 from systems.forecast_combine import ForecastCombine
-from private.systems.carrytrend.forecastScaleCap import volAttenForecastScaleCap
-from private.systems.carrytrend.rawdata import myFuturesRawData
+from systems.rob_system.forecastScaleCap import volAttenForecastScaleCap
+from systems.rob_system.rawdata import myFuturesRawData
 from systems.positionsizing import PositionSizing
 from systems.portfolio import Portfolios
 from systems.provided.dynamic_small_system_optimise.portfolio_weights_stage import portfolioWeightsStage
@@ -50,10 +15,12 @@ from systems.provided.dynamic_small_system_optimise.optimised_positions_stage im
 from systems.provided.dynamic_small_system_optimise.risk import Risk
 from systems.provided.dynamic_small_system_optimise.accounts_stage import accountForOptimisedStage
 
-def futures_system(sim_data = arg_not_supplied, config_filename = ""):
+def futures_system(sim_data = arg_not_supplied,
+                   config_filename = "systems.rob_system.config.yaml"):
+    
     if sim_data is arg_not_supplied:
+        sim_data = csvFuturesSimData()
 
-    sim_data = get_sim_data_object_for_production(data)
     config = Config(config_filename)
 
     system = System(
@@ -70,7 +37,7 @@ def futures_system(sim_data = arg_not_supplied, config_filename = ""):
             Rules(),
 
         ],
-        data,
+        sim_data,
         config,
     )
     system.set_logging_level("on")
