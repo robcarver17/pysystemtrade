c8a994c4c2530a4fa3810a83b1391608ad847de8
rob <robcarver17@googlemail.com>
2021-12-01 11:38:09 +0000

    started moving my system yaml to public

--- systems/provided/rob_system/run_system.py
+++ systems/provided/rob_system/run_system.py
@@ -1,13 +1,14 @@
 
 from syscore.objects import arg_not_supplied
-from sysdata.sim.csv_futures_sim_data import csvFuturesSimData
+#from sysdata.sim.csv_futures_sim_data import csvFuturesSimData
+from sysdata.sim.db_futures_sim_data import dbFuturesSimData
 from sysdata.config.configdata import Config
 
 from systems.forecasting import Rules
 from systems.basesystem import System
 from systems.forecast_combine import ForecastCombine
-from systems.rob_system.forecastScaleCap import volAttenForecastScaleCap
-from systems.rob_system.rawdata import myFuturesRawData
+from systems.provided.rob_system.forecastScaleCap import volAttenForecastScaleCap
+from systems.provided.rob_system.rawdata import myFuturesRawData
 from systems.positionsizing import PositionSizing
 from systems.portfolio import Portfolios
 from systems.provided.dynamic_small_system_optimise.portfolio_weights_stage import portfolioWeightsStage
@@ -16,10 +17,10 @@ from systems.provided.dynamic_small_system_optimise.risk import Risk
 from systems.provided.dynamic_small_system_optimise.accounts_stage import accountForOptimisedStage
 
 def futures_system(sim_data = arg_not_supplied,
-                   config_filename = "systems.rob_system.config.yaml"):
-    
+                   config_filename = "systems.provided.rob_system.config.yaml"):
+
     if sim_data is arg_not_supplied:
-        sim_data = csvFuturesSimData()
+        sim_data = dbFuturesSimData()
 
     config = Config(config_filename)
 
