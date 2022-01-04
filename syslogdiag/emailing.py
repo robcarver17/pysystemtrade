56fd640d1397fac8fc94d96a4a2ab6db25c51ab0
Chris Allison <chris@allison.nu>
2022-01-04 11:59:52 +0100

    emailing file cahnges and typo in docs

--- syslogdiag/emailing.py
+++ syslogdiag/emailing.py
@@ -68,21 +68,27 @@ def _send_msg(msg):
 
     """
 
-    email_server, email_address, email_pwd, email_to, email_port = get_email_details()
+    email_server, email_address, email_pwd, email_to, email_port, use_gmail = get_email_details()
 
     me = email_address
     you = email_to
     msg["From"] = me
     msg["To"] = you
 
+
     # Send the message via our own SMTP server, but don't include the
     # envelope header.
-    s = smtplib.SMTP(email_server, email_port)
-    # add tls for those using yahoo or gmail. 
-    try:
-        s.starttls()
-    except:
-        pass
+    if use_gmail:
+        s = smtplib.SMTP_SSL(email_server, email_port)
+    else:
+        s = smtplib.SMTP(email_server, email_port)
+    # add tls for those using yahoo or gmail.
+    
+    if use_gmail:
+        try:
+            s.starttls()
+        except:
+            pass
     s.login(email_address, email_pwd)
     s.sendmail(me, [you], msg.as_string())
     s.quit()
@@ -97,7 +103,9 @@ def get_email_details():
         email_server = production_config.email_server
         email_to = production_config.email_to
         email_port = production_config.email_port
+        use_gmail = production_config.use_gmail
+
     except:
         raise Exception("Need to have all of these for email to work in private config: email_address, email_pwd, email_server, email_to", "email_port")
 
-    return email_server, email_address, email_pwd, email_to, email_port
+    return email_server, email_address, email_pwd, email_to, email_port, use_gmail
