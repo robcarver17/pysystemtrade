3f33ef259a950239253f67b71297951983719b62
LangleJ <john.langworthy@hotmail.co.uk>
2022-01-09 19:53:52 +0000

    Added ability to run optimiseWeightsOverTime.weights() under a multiprocessing pool with imap.
    This provides a substantial speed improvement.
    
    To enable, add n_threads = N  to config.forecast_weight_estimate.

--- sysquant/returns.py
+++ sysquant/returns.py
@@ -54,6 +54,12 @@ class returnsForOptimisation(pd.DataFrame):
         super().__init__(*args, **kwargs)
         self._frequency = frequency
         self._pooled_length = pooled_length
+        # Any additional attributes need to be added into the reduce below
+
+    def __reduce__(self):
+        t = super().__reduce__()
+        t[2].update( {'_is_copy':self._is_copy, '_frequency':self._frequency, '_pooled_length':self._pooled_length } )
+        return t[0], t[1], t[2]
 
     @property
     def frequency(self):
