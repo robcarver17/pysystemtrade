ca6e4f8a93a2837f394eb86ff42f1d8f9c633c4e
rob <robcarver17@googlemail.com>
2022-03-23 15:38:03 +0000

    sr cost breakdown

--- sysproduction/reporting/costs_report.py
+++ sysproduction/reporting/costs_report.py
@@ -6,7 +6,7 @@ from syscore.objects import header, table, arg_not_supplied, body_text
 from sysproduction.reporting.api import reportingApi
 
 COSTS_REPORT_TEXT = body_text(
-    "Cost calculations: Costs shown are in SR units and are calculated as follows:"
+    "Cost calculations: Costs shown are in SR units and are calculated as follows: percentage_cost /  avg_annual_vol_perc "
 )
 
 def costs_report(
