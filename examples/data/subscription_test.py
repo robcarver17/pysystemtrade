f3abde484959c6fe0a3313651f015243eef86f64
js190 <js190@peace>
2022-07-17 22:04:41 +0100

    fix

--- examples/data/subscription_test.py
+++ examples/data/subscription_test.py
@@ -11,20 +11,20 @@ from ib_insync import IB
 from sysbrokers.IB.ib_instruments_data import IBconfig
 from sysbrokers.IB.ib_instruments_data import read_ib_config_from_file
 
-HOSTNAME = "localhost"
+HOSTNAME = "bertha.local"
 
 
-async def subscription_test(
-    ib_symbols: List[str], ib_config: IBconfig
-) -> Dict[str, List[ContractDetails]]:
+async def subscription_test(ib_config: IBconfig) -> Dict[str, List[ContractDetails]]:
     ib_insync.util.logToConsole(logging.INFO)
     ib = IB()
     failed = []
     exceptions = []
+    ib_symbols: List[str] = ib_config.IBSymbol.tolist()
+    instruments: List[str] = ib_config.Instrument.tolist()
     try:
         await ib.connectAsync(host=HOSTNAME, port=4001, clientId=1)
         coros = []
-        for ib_symbol in ib_symbols:
+        for i, ib_symbol in enumerate(ib_symbols):
             try:
                 ib_exchange = ib_config[
                     ib_config.IBSymbol == ib_symbol
@@ -32,7 +32,7 @@ async def subscription_test(
                 f = Future(symbol=ib_symbol, exchange=ib_exchange, includeExpired=True)
                 coros.append(ib.reqContractDetailsAsync(f))
             except Exception as e:
-                failed.append(ib_symbol)
+                failed.append(instruments[i])
                 exceptions.append(str(e))
         results = await asyncio.gather(*coros)
         data = {ib: sorted_ContractDetails(cd) for ib, cd in zip(ib_symbols, results)}
@@ -47,7 +47,7 @@ def sorted_ContractDetails(contract_details: List[ContractDetails]):
 
 def main():
     ib_config: IBconfig = read_ib_config_from_file()
-    return asyncio.run(subscription_test(ib_config.IBSymbol.tolist(), ib_config))
+    return asyncio.run(subscription_test(ib_config))
 
 
 if __name__ == "__main__":
