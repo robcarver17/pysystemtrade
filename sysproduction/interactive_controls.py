cd95efe4d0f72a6548aa52713b198f87ff23d293
todd <tgibson11@gmail.com>
2022-12-20 06:34:08 -0700

    Temporary workaround
    (cherry picked from commit 216db77a65b97d2bc4af90c4ede81b0e1190a15b)

--- sysproduction/interactive_controls.py
+++ sysproduction/interactive_controls.py
@@ -972,9 +972,10 @@ def check_price_multipliers_consistent_for_instrument(data: dataBlob,
 
 def delete_instrument_from_prices(data: dataBlob):
     exit_code=""
-    instrument_code = get_valid_instrument_code_from_user(
-        allow_all=False, source = "single", allow_exit=True,
-    exit_code=exit_code)
+    # instrument_code = get_valid_instrument_code_from_user(
+    #     allow_all=False, source = "single", allow_exit=True,
+    # exit_code=exit_code)
+    instrument_code = "LUMBER"
 
     if instrument_code == exit_code:
         return False
