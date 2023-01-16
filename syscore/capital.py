f06895aa410416e9b7d59a8ba479a52060ac5695
rob <robcarver17@googlemail.com>
2023-01-16 16:28:24 +0000

    tidy up and annotation of capital multiplier code

--- syscore/capital.py
+++ syscore/capital.py
@@ -1,21 +1,30 @@
 """
+WARNING DO NOT REFACTOR NAMES OR LOCATION OF THIS CODE AS IT IS USED IN .YAML FILES, eg
+
+capital_multiplier:
+   func: syscore.capital.fixed_capital
+
 Functions to calculate capital multiplier
 
-ALl return Tx1 pd.Series
+ALl return Tx1 pd.Series, where a value of 1 is the original capital
+
+See https://qoppac.blogspot.com/2016/06/capital-correction-pysystemtrade.html for more details
+
 """
 from copy import copy
 import pandas as pd
 import numpy as np
+from systems.basesystem import System
 
 
-def fixed_capital(system, **ignored_args):
+def fixed_capital(system: System, **ignored_args) -> pd.Series:
     multiplier = copy(system.accounts.portfolio().percent)
     multiplier[:] = 1.0
 
     return multiplier
 
 
-def full_compounding(system, **ignored_args):
+def full_compounding(system: System, **ignored_args) -> pd.Series:
     pandl = system.accounts.portfolio().percent
     multiplier = 1.0 + (pandl / 100.0)
     multiplier = multiplier.cumprod().ffill()
@@ -23,7 +32,9 @@ def full_compounding(system, **ignored_args):
     return multiplier
 
 
-def half_compounding(system, **ignored_args):
+def half_compounding(system: System, **ignored_args) -> pd.Series:
+
+    ## remove any nans
     pandl = system.accounts.portfolio().percent.curve().ffill().diff()
     multiplier = 1.0
     multiplier_list = []
