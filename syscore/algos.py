00759e36d958b5454d70f36a78d7964727566f6c
rob <robcarver17@googlemail.com>
2023-01-16 15:34:13 +0000

    refactored syscore algos calcualte weighted average

--- syscore/algos.py
+++ syscore/algos.py
@@ -5,38 +5,92 @@ Basic building blocks of trading rules, like volatility measurement and
 crossovers
 
 """
-import warnings
 import math as maths
 import numpy as np
 import pandas as pd
 
 from syscore.genutils import sign
+from copy import copy
 
 
-LARGE_NUMBER_OF_DAYS = 250 * 100 * 100
+def calculate_weighted_average_with_nans(
+    weights: list, list_of_values: list, sum_of_weights_should_be: float = 1.0
+) -> float:
+
+    """
+    Calculate a weighted average when the weights and/or values might be nans
+    >>> calculate_weighted_average_with_nans([0.2, 0.2, np.nan, 0.4],[2, np.nan, 3, np.nan])
+    2.0
+    >>> calculate_weighted_average_with_nans([np.nan, 0.2, np.nan, 0.4],[2, np.nan, 3, np.nan])
+    0.0
+
+    """
+    ## easier to work in np space
+    np_weights = np.array(weights)
+    np_values = np.array(list_of_values)
+
+    # get safe weights
+    np_weights_without_nans_in_weights_or_values = (
+        calculate_np_weights_without_nans_in_weights_or_values(
+            np_weights=np_weights, np_values=np_values
+        )
+    )
+
+    normalised_weights = renormalise_array_of_values_to_sum(
+        np_weights_without_nans_in_weights_or_values,
+        renormalise_sum_to=sum_of_weights_should_be,
+    )
+
+    weighted_value = calculate_weighted_average(
+        np_weights=normalised_weights, np_values=np_values
+    )
+
+    return weighted_value
 
 
-def calculate_weighted_average_with_nans(weights: list, data: list):
-    def _zero_weight(weight, data_item):
-        if np.isnan(weight) or np.isnan(data_item):
-            return 0.0
-        return weight
+def calculate_np_weights_without_nans_in_weights_or_values(
+    np_weights: np.array, np_values: np.array
+) -> np.array:
+    """
+    Get set of weights where neithier the value or the weight is nan
+    >>> calculate_np_weights_without_nans_in_weights_or_values(np.array([0.2, 0.2, np.nan, 0.4]),np.array([2, np.nan, 3, np.nan]))
+    array([0.2, 0. , 0. , 0. ])
+    """
 
-    safe_weights = [
-        _zero_weight(weight, data_item) for weight, data_item in zip(weights, data)
-    ]
+    weights_times_values_as_np = np_weights * np_values
+    empty_weights = np.isnan(weights_times_values_as_np)
 
-    sum_safe_weights = sum(safe_weights)
-    if sum_safe_weights == 0:
-        return 0.0
-    renormalise = 1.0 / sum_safe_weights
-    weighted_value = [
-        renormalise * weight * data_item
-        for weight, data_item in zip(safe_weights, data)
-    ]
+    weights_without_nan = copy(np_weights)
+    weights_without_nan[empty_weights] = 0.0
 
-    # could still be nan in data
-    return np.nansum(weighted_value)
+    return weights_without_nan
+
+
+def renormalise_array_of_values_to_sum(
+    array_of_values: np.array, renormalise_sum_to: float = 1.0
+) -> np.array:
+    """
+    Most commonly used to renormalise weights to 1.0
+
+    >>> renormalise_array_of_values_to_sum(np.array([0.125, 0.125, 0.25, 0.0]), 2.0)
+    array([0.5, 0.5, 1. , 0. ])
+
+    """
+    sum_of_values = np.nansum(array_of_values)
+    renormalise_multiplier = renormalise_sum_to / sum_of_values
+    new_values = array_of_values * renormalise_multiplier
+
+    return new_values
+
+
+def calculate_weighted_average(np_weights: np.array, np_values: np.array) -> float:
+    """
+    >>> calculate_weighted_average(np.array([0.2, 0.2, 0, 0.4]),np.array([2, np.nan, 3, np.nan]))
+    0.4
+    """
+    weights_times_values_as_np = np_weights * np_values
+
+    return np.nansum(weights_times_values_as_np)
 
 
 def apply_with_min_periods(xcol, my_func=np.nanmean, min_periods=0):
---
+++ systems/accounts/curves/account_curve_ttest.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from typing import Union, NamedTuple
+from scipy.stats import ttest_rel
+
+from systems.accounts.curves.account_curve import accountCurve
+
+
+class AccountTTestResult(NamedTuple):
+    """Structure to store the results of the t-test"""
+
+    diff: float
+    statistic: float
+    pvalue: float
+
+
+def account_t_test(
+    acc1: Union[accountCurve, pd.Series], acc2: Union[accountCurve, pd.Series]
+) -> AccountTTestResult:
+    """
+    Given two accountCurve-like objects, performs a two-sided t-test on normalised returns.
+
+    Parameters
+    ----------
+    acc1 : accountCurve or pd.Series
+        first set of returns
+    acc2 : accountCurve or pd.Series
+        second set of returns
+
+    Returns
+    -------
+    diff : float
+        difference in standardised means of returns
+    statistic : float
+        t-statistic for the t-test
+    pvalue : float
+        p-value for the t-test
+    """
+
+    standardised_df_returns = _prepare_returns_data_for_t_test(acc1=acc1, acc2=acc2)
+
+    means = standardised_df_returns.means(axis=0).values
+    returns = standardised_df_returns.values
+
+    # Get the difference in (standardised) means
+    diff = means[0] - means[1]
+
+    # Perform the two-sided t-test
+    ttest_results = ttest_rel(returns[:, 0], returns[:, 1], nan_policy="omit")
+    t = ttest_results.statistic
+    pvalue = ttest_results.pvalue
+
+    return AccountTTestResult(diff, t, pvalue)
+
+
+def _prepare_returns_data_for_t_test(
+    acc1: Union[accountCurve, pd.Series], acc2: Union[accountCurve, pd.Series]
+) -> pd.DataFrame:
+    # Inner join on the cumulative returns
+    acc1 = pd.DataFrame(acc1).cumsum()
+    acc2 = pd.DataFrame(acc2).cumsum()
+    cum = pd.merge(acc1, acc2, left_index=True, right_index=True)
+    cum = cum.sort_index(ascending=True, inplace=False)
+
+    # Get returns, standardise and compute means
+    df_returns = cum.diff(periods=1).dropna(how="any")
+    standardised_df_returns = df_returns / (df_returns.std(axis=0) + 1e-100)
+
+    return standardised_df_returns
