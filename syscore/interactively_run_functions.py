0dac32c4b8899feba6285a1cdf1d0a0cb2a898ec
rob <robcarver17@googlemail.com>
2023-01-18 16:22:53 +0000

    run functions

--- syscore/interactively_run_functions.py
+++ syscore/interactively_run_functions.py
@@ -1,40 +1,41 @@
 import inspect
+from typing import Union
 
 NO_DEFAULT = object()
 NO_TYPE_PROVIDED = object()
 NO_VALID_ARGUMENT_PASSED = object()
+EMPTY_VALUE = inspect._empty
 
 
-def parameter_default(parameter_signature):
-    default = parameter_signature.default
-    if default is inspect._empty:
-        default = NO_DEFAULT
-    return default
+def interactively_input_arguments_for_function(func, full_funcname):
+    """
+    Prints the docstring of func, then asks for all of its arguments with defaults
 
+    Optionally casts to type, if any argument name is an entry in the dict type_casting
+    """
 
-def has_default(parameter_signature):
-    return parameter_default(parameter_signature) is not NO_DEFAULT
+    func_arguments = inspect.signature(func).parameters
 
+    print(
+        "\n %s:\n %s \n Arguments: %s"
+        % (full_funcname, inspect.getdoc(func), list(func_arguments.keys()))
+    )
 
-def parameter_type(parameter_signature):
-    ptype = parameter_signature.annotation
-    if ptype is inspect._empty:
-        # get from default
-        if has_default(parameter_signature):
-            default_value = parameter_default(parameter_signature)
-            ptype = type(default_value)
+    args = []
+    kwargs = dict()
+    for (argname, parameter_signature) in func_arguments.items():
+        arg_value = input_and_type_cast_argument(argname, parameter_signature)
+
+        is_kwarg = has_default(parameter_signature)
+        if is_kwarg:
+            kwargs[argname] = arg_value
         else:
-            # give up
-            return NO_TYPE_PROVIDED
+            args.append(arg_value)
 
-    return ptype.__name__
-
-
-def has_type(parameter_signature):
-    return parameter_type(parameter_signature) is not NO_TYPE_PROVIDED
+    return args, kwargs
 
 
-def input_and_type_cast_argument(argname, parameter_signature):
+def input_and_type_cast_argument(argname: str, parameter_signature: inspect.Parameter):
     """
     Interactively get a value for a parameter, considering any type casting required or defaults
     :param argname: str
@@ -100,37 +101,42 @@ def input_and_type_cast_argument(argname, parameter_signature):
     return arg_value
 
 
-def fill_args_and_run_func(func, full_funcname):
-    """
-    Prints the docstring of func, then asks for all of its arguments with defaults
+def has_type(parameter_signature):
+    return parameter_type(parameter_signature) is not NO_TYPE_PROVIDED
 
-    Optionally casts to type, if any argument name is an entry in the dict type_casting
-    """
-    # print doc string
 
-    print("\n")
-    print(full_funcname + ":")
-    print(inspect.getdoc(func))
-    print("\n")
-    func_arguments = inspect.signature(func).parameters
+def parameter_type(
+    parameter_signature: inspect.Parameter,
+) -> Union[str, NO_TYPE_PROVIDED]:
 
-    print("\nArguments:")
-    print(list(func_arguments.keys()))
-    print("\n")
-
-    args = []
-    kwargs = dict()
-    for (argname, parameter_signature) in func_arguments.items():
-        arg_value = input_and_type_cast_argument(argname, parameter_signature)
-        if arg_value is NO_VALID_ARGUMENT_PASSED:
-            raise Exception(
-                "Invalid argument passed - should not happen - check function 'input_and_type_cast_argument' logic"
-            )
-
-        is_kwarg = has_default(parameter_signature)
-        if is_kwarg:
-            kwargs[argname] = arg_value
+    ptype = parameter_signature.annotation
+    if ptype is EMPTY_VALUE:
+        # get from default
+        if has_default(parameter_signature):
+            name_of_type = get_name_of_default_type(parameter_signature)
         else:
-            args.append(arg_value)
+            # give up
+            return NO_TYPE_PROVIDED
+    else:
+        name_of_type = ptype.__name__
 
-    return args, kwargs
+    return name_of_type
+
+
+def get_name_of_default_type(parameter_signature: inspect.Parameter) -> str:
+    default_value = parameter_default(parameter_signature)
+    default_ptype = type(default_value)
+    name_of_type = default_ptype.__name__
+
+    return name_of_type
+
+
+def has_default(parameter_signature: inspect.Parameter) -> bool:
+    return parameter_default(parameter_signature) is not NO_DEFAULT
+
+
+def parameter_default(parameter_signature: inspect.Parameter):
+    default = parameter_signature.default
+    if default is EMPTY_VALUE:
+        default = NO_DEFAULT
+    return default
