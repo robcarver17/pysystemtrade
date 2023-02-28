1ceb2873b004c33b24b202859a9ad00dbdd165af
rob <robcarver17@googlemail.com>
2023-02-28 09:21:39 +0000

    some more work on #1020

--- sysinit/futures/seed_price_data_from_IB.py
+++ sysinit/futures/seed_price_data_from_IB.py
@@ -3,53 +3,62 @@ from sysbrokers.IB.ib_futures_contract_price_data import (
     futuresContract,
 )
 from syscore.dateutils import DAILY_PRICE_FREQ, HOURLY_FREQ, Frequency
+from sysdata.data_blob import dataBlob
+
 from sysproduction.data.broker import dataBroker
 from sysproduction.data.prices import updatePrices
-from sysinit.futures.create_hourly_and_daily import write_split_data_for_instrument
-
+from sysproduction.update_historical_prices import write_merged_prices_for_contract
 
 def seed_price_data_from_IB(instrument_code):
-    data_broker = dataBroker()
+    data = dataBlob()
+    data_broker = dataBroker(data)
 
     list_of_contracts = data_broker.get_list_of_contract_dates_for_instrument_code(
         instrument_code, allow_expired=True
     )
 
-    for contract in list_of_contracts:
-        seed_price_data_for_contract(contract)
+    for contract_date in list_of_contracts:
+        contract_object = futuresContract(instrument_code, contract_date)
+        seed_price_data_for_contract(data=data, contract_object=contract_object)
 
-    write_split_data_for_instrument(instrument_code)
 
 
-def seed_price_data_for_contract( contract: futuresContract):
-    ## We do this slightly tortorous thing because there are energy contracts
-    ## which don't expire in the month they are labelled with
-    ## So for example, CRUDE_W 202106 actually expires on 20210528
+def seed_price_data_for_contract(data: dataBlob, contract_object: futuresContract):
+    list_of_frequencies= [DAILY_PRICE_FREQ, HOURLY_FREQ]
     for frequency in [DAILY_PRICE_FREQ, HOURLY_FREQ]:
-        seed_price_data_for_contract_at_frequency(contract, frequency=frequency)
+        seed_price_data_for_contract_at_frequency(data=data, contract_object=contract_object, frequency=frequency)
 
-def seed_price_data_for_contract_at_frequency( contract: futuresContract, frequency: Frequency):
+    write_merged_prices_for_contract(
+        data, contract_object=contract_object, list_of_frequencies=list_of_frequencies
+    )
+
+
+def seed_price_data_for_contract_at_frequency(data: dataBlob, contract_object: futuresContract, frequency: Frequency):
+
+    data_broker = dataBroker(data)
+    update_prices = updatePrices(data)
+
     ## We do this slightly tortorous thing because there are energy contracts
     ## which don't expire in the month they are labelled with
     ## So for example, CRUDE_W 202106 actually expires on 20210528
 
-    data_broker = dataBroker()
-    update_prices = updatePrices()
+    date_str = contract_object.contract_date.date_str[:6]
+    new_contract = futuresContract(contract_object.instrument, date_str)
 
-    date_str = contract.contract_date.date_str[:6]
-    new_contract = futuresContract(contract.instrument, date_str)
+    log = new_contract.specific_log(data.log)
     try:
         prices = data_broker.get_prices_at_frequency_for_potentially_expired_contract_object(
             new_contract,
             frequency=frequency
         )
     except missingData:
-        prices = []
+        log.warn("Error getting data for %s" % str(new_contract))
+        return None
 
     if len(prices) == 0:
-        print("No data!")
+        log.warn("No price data for %s" % str(new_contract))
     else:
-        update_prices.overwrite_prices_at_frequency_for_contract(contract_object=contract,
+        update_prices.overwrite_prices_at_frequency_for_contract(contract_object=contract_object,
                                                                  frequency=frequency,
                                                                  new_prices=prices)
 
