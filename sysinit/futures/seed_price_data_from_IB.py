3d289a451921913386e9f9e62afaf7ac02f6fced
rob <robcarver17@googlemail.com>
2023-02-28 09:25:04 +0000

    some more work on #1020

--- sysinit/futures/seed_price_data_from_IB.py
+++ sysinit/futures/seed_price_data_from_IB.py
@@ -24,12 +24,16 @@ def seed_price_data_from_IB(instrument_code):
 
 
 def seed_price_data_for_contract(data: dataBlob, contract_object: futuresContract):
+    log = contract_object.specific_log(data.log)
+
     list_of_frequencies= [HOURLY_FREQ, DAILY_PRICE_FREQ]
     for frequency in list_of_frequencies:
+        log.msg("Getting data at frequency %s" % str(frequency))
         seed_price_data_for_contract_at_frequency(data=data,
                                                   contract_object=contract_object,
                                                   frequency=frequency)
 
+    log.msg("Writing merged data for %s" % str(contract_object))
     write_merged_prices_for_contract(
         data, contract_object=contract_object, list_of_frequencies=list_of_frequencies
     )
