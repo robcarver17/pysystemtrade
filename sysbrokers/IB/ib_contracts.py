4092a0131af092dd33f81a72bd036bd0d57924aa
rob <robcarver17@googlemail.com>
2023-03-20 15:54:18 +0000

    reinstate caching and fix up methods

--- sysbrokers/IB/ib_contracts.py
+++ sysbrokers/IB/ib_contracts.py
@@ -1,5 +1,5 @@
 from ib_insync import Contract as ibContract, Contract, ComboLeg
-
+from ib_insync import ContractDetails as ibContractDetails
 import re
 
 from sysbrokers.IB.ib_instruments import (
