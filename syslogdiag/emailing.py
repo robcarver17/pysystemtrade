0a9b4747352c89ad4c46f5ad838e8aa6f3955728
rob <robcarver17@googlemail.com>
2023-03-24 08:36:45 +0000

    we sacrifice to the laws of black

--- syslogdiag/emailing.py
+++ syslogdiag/emailing.py
@@ -87,7 +87,7 @@ def _send_msg(msg: MIMEMultipart):
     # Send the message via our own SMTP server, but don't include the
     # envelope header.
     s = smtplib.SMTP(email_server, email_port)
-    context=ssl.create_default_context()
+    context = ssl.create_default_context()
     # add tls for those using yahoo or gmail.
     try:
         s.starttls(context=context)
