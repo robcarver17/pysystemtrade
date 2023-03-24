5732742ad0a007eb1e75f896a476b9c2b7941316
rob <robcarver17@googlemail.com>
2023-03-24 15:34:03 +0000

    finished text logging

--- syslogdiag/log_to_file.py
+++ syslogdiag/log_to_file.py
@@ -1,15 +1,17 @@
+import os
 from os.path import join as join_file_and_path
-from syscore.constants import missing_data
-from syscore.fileutils import get_resolved_pathname
+
+from syscore.constants import missing_data, missing_file
+from syscore.fileutils import get_resolved_pathname, does_resolved_filename_exist
 
 from sysdata.config.production_config import get_production_config
-from sysdata.data_blob import dataBlob
 
 from syslogdiag.pst_logger import pst_logger, DEFAULT_LOG_LEVEL
 from syslogdiag.log_entry import logEntry
 from syslogdiag.email_via_db_interface import send_production_mail_msg
 
 EMAIL_ON_LOG_LEVEL = [4]
+LINES_TO_FLUSH_AFTER = 5
 
 
 class logToFile(pst_logger):
@@ -41,7 +43,7 @@ class logToFile(pst_logger):
         log_entry = logEntry(
             text, msglevel=msglevel, attributes=attributes, log_id=log_id
         )
-        print(log_entry)
+        self.log_to_file(log_entry)
 
         if msglevel in EMAIL_ON_LOG_LEVEL:
             # Critical, send an email
@@ -50,7 +52,33 @@ class logToFile(pst_logger):
         return log_entry
 
     def log_to_file(self, log_entry: logEntry):
-        self.log_file_handle.write(str(log_entry))
+        self.log_file_handle.write("%s\n" % str(log_entry))
+        self.check_if_ready_and_if_so_flush()
+
+    def check_if_ready_and_if_so_flush(self):
+        log_id = self.log_id
+        if (log_id % LINES_TO_FLUSH_AFTER) == 0:
+            self.log_file_handle.flush()
+
+    def get_next_log_id(self) -> int:
+        current_log_id = self.log_id
+        incremented_log_id = current_log_id + 1
+        self._logid = incremented_log_id
+
+        return incremented_log_id
+
+    @property
+    def log_id(self) -> int:
+        current_log_id = getattr(self, "_logid", None)
+        if current_log_id is None:
+            current_log_id = 0
+            self.log_id = current_log_id
+
+        return current_log_id
+
+    @log_id.setter
+    def log_id(self, log_id: int):
+        self._logid = log_id
 
     def email_user(self, log_entry: logEntry):
         data = self.data
@@ -69,12 +97,15 @@ class logToFile(pst_logger):
         else:
             file_handle.close()
 
+            ## force reopen on new log
+            self._file_handle = missing_file
+
     @property
     def log_file_handle(self):
-        file_handle = getattr(self, "_file_handle", None)
-        if file_handle is None:
+        file_handle = getattr(self, "_file_handle", missing_file)
+        if file_handle is missing_file:
             filename = self.log_filename_with_path
-            file_handle = open(filename, "w")
+            file_handle = open(filename, "a", 5)
 
             self._file_handle = file_handle
 
@@ -98,9 +129,9 @@ class logToFile(pst_logger):
         return self.attributes["type"]
 
 
-def get_logging_directory(data: dataBlob):
-    config = get_production_config(data)
-    log_dir = config.get_element_or_return_default("log_directory", None)
+def get_logging_directory(data: "dataBlob"):
+    config = get_production_config()
+    log_dir = config.get_element_or_default("log_directory", None)
     if log_dir is None:
         ## Can't log this but print anyway
         print(
@@ -109,5 +140,9 @@ def get_logging_directory(data: dataBlob):
         return missing_data
 
     log_dir = get_resolved_pathname(log_dir)
+    try:
+        os.mkdir(log_dir)
+    except:
+        pass
 
     return log_dir
