ef3dde2fad010dfdad9d26d42cdd8f47503fa616
todd <3578666+tgibson11@users.noreply.github.com>
2023-04-19 06:30:02 -0700

    Use Pandas NaT for missing control start/end time

--- syscore/pandas/pdutils.py
+++ syscore/pandas/pdutils.py
@@ -300,11 +300,9 @@ def make_df_from_list_of_named_tuple(
 
 
 def sort_df_ignoring_missing(df: pd.DataFrame, column: List[str]) -> pd.DataFrame:
-    # sorts df by column, with rows containing missing_data coming at the end
-    missing = df[df[column] == missing_data]
-    valid = df[df[column] != missing_data]
-    valid_sorted = valid.sort_values(column)
-    return pd.concat([valid_sorted, missing])
+    # sorts df by column, with rows with missing data coming at the end
+    # Pandas treats NaN, NaT, and None as missing
+    return df.sort_values(column, na_position="last")
 
 
 def apply_with_min_periods(
