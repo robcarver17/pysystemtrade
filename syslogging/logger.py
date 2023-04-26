41b5a884a45cfee20fb27b4c4bfb33ee968d0cb3
Andy Geach <andy@bugorfeature.net>
2023-04-26 15:10:39 +0100

    better logging init for sim. in prod, we check for open log server socket on startup

--- syslogging/logger.py
+++ syslogging/logger.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import socket
 import logging.config
 import syslogging
 from syslogging.adapter import *
@@ -29,7 +30,7 @@ def logtoscreen(name="", **kwargs):
     if name is None or name == "":
         if "type" in kwargs:
             name = kwargs["type"]
-    return DynamicAttributeLogger(logging.getLogger(name), kwargs)
+    return get_logger(name, kwargs)
 
 
 def nullLog(name):
@@ -61,7 +62,7 @@ def _configure_logging():
 
 
 def _configure_sim():
-    print(f"Attempting to configure sim logging")
+    print(f"Configuring sim logging")
     handler = logging.StreamHandler(stream=sys.stdout)
     handler.setLevel(logging.DEBUG)
     logging.getLogger("ib_insync").setLevel(logging.WARNING)
@@ -82,13 +83,36 @@ def _configure_prod(logging_config_file):
     if os.path.exists(config_path):
         try:
             config = parse_config(path=config_path)
+            host, port = _get_log_server_config(config)
+            try:
+                _check_log_server(host, port)
+            except BlockingIOError:
+                print(f"Log server detected OK at {host}:{port}")
+            except (ConnectionResetError, ConnectionRefusedError):
+                print(f"Cannot connect to log server at {host}:{port}, is it running?")
+                raise
             logging.config.dictConfig(config)
             syslogging.logging_configured = True
         except Exception as exc:
-            print(f"ERROR: Problem configuring logging, reverting to basic: {exc}")
+            print(f"ERROR: Problem configuring prod logging, reverting to sim: {exc}")
             _configure_sim()
     else:
-        print(
-            f"ERROR: logging config '{config_path}' does not exist, reverting to basic"
-        )
+        print(f"ERROR: prod logging config '{config_path}' not found, reverting to sim")
         _configure_sim()
+
+
+def _get_log_server_config(config):
+    handler = config["handlers"]["socket"]
+    return handler["host"], handler["port"]
+
+
+def _check_log_server(host, port):
+    # https://stackoverflow.com/questions/48024720/python-how-to-check-if-socket-is-still-connected
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    # this will try to read bytes without blocking and also without removing them
+    # from buffer (peek only). If a BlockingIOError is raised, we know the socket
+    # is open
+    data = sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
+    if len(data) == 0:
+        raise Exception("Unexpected data length")
