fde55f4cf512b009011735404b47b62b88b34f7f
Andy Geach <andy@bugorfeature.net>
2023-04-27 16:26:37 +0100

    Remove older queue based prod logging

--- syslogging/handlers.py
+++ syslogging/handlers.py
@@ -1,66 +1,9 @@
 import logging
-from queue import Queue
 from collections import deque
-import atexit
 import pickle
 import socketserver
 import struct
-from logging.config import ConvertingList, ConvertingDict, valid_ident
-from logging.handlers import QueueHandler, QueueListener
 from syslogdiag.emailing import send_mail_msg
-
-
-def _resolve_handlers(l):
-    if not isinstance(l, ConvertingList):
-        return l
-
-    # Indexing the list performs the evaluation.
-    return [l[i] for i in range(len(l))]
-
-
-def _resolve_queue(q):
-    if not isinstance(q, ConvertingDict):
-        return q
-    if "__resolved_value__" in q:
-        return q["__resolved_value__"]
-
-    cname = q.pop("class")
-    klass = q.configurator.resolve(cname)
-    props = q.pop(".", None)
-    kwargs = {k: q[k] for k in q if valid_ident(k)}
-    result = klass(**kwargs)
-    if props:
-        for name, value in props.items():
-            setattr(result, name, value)
-
-    q["__resolved_value__"] = result
-    return result
-
-
-class QueueListenerHandler(QueueHandler):
-
-    # Gratefully stolen from https://github.com/rob-blackbourn/medium-queue-logging
-
-    def __init__(
-        self, handlers, respect_handler_level=False, auto_run=True, queue=Queue(-1)
-    ):
-        queue = _resolve_queue(queue)
-        super().__init__(queue)
-        handlers = _resolve_handlers(handlers)
-        self._listener = QueueListener(
-            self.queue, *handlers, respect_handler_level=respect_handler_level
-        )
-        if auto_run:
-            self.start()
-            atexit.register(self.stop)
-
-    def start(self):
-        print("Starting logging queue listener...")
-        self._listener.start()
-
-    def stop(self):
-        print("Shutting down logging queue listener\n")
-        self._listener.stop()
 
 
 class PstSMTPHandler(logging.Handler):
