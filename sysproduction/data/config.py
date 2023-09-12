87966c4ddc15ff495abe6ee0f9477a62b8a82e49
rob <robcarver17@googlemail.com>
2023-09-12 09:15:57 +0100

    blacked

--- sysproduction/data/config.py
+++ sysproduction/data/config.py
@@ -53,10 +53,12 @@ def get_list_of_stale_instruments() -> list:
     config = get_production_config()
     return get_list_of_stale_instruments_given_config(config)
 
+
 def get_list_of_stale_instruments_given_config(config) -> list:
     stale_instruments = config.get_element_or_default("stale_instruments", [])
 
     return stale_instruments
+
 
 def get_list_of_stale_strategies() -> list:
     config = get_production_config()
