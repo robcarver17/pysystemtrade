7708841bc2ae8a3d1af3bc8f929119179dd4dada
Andy Geach <andy@bugorfeature.net>
2023-10-10 22:40:27 +0100

    removing unnecessary use of log.setup(), logger is set up in __init__()

--- sysdata/config/configdata.py
+++ sysdata/config/configdata.py
@@ -189,10 +189,6 @@ class Config(object):
         :return: nothing
         """
 
-        # inherit the log
-        # TODO log.setup
-        setattr(self, "log", base_system.log.setup(stage="config"))
-
         # fill with defaults
         self.fill_with_defaults()
 
