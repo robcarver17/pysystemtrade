35add5533da4e0aa62a386fc2b000642371c3513
Andy Geach <andy@bugorfeature.net>
2023-10-13 13:00:49 +0100

    updating logging TODOs

--- syslogging/adapter.py
+++ syslogging/adapter.py
@@ -9,9 +9,6 @@ class DynamicAttributeLogger(logging.LoggerAdapter):
 
     """
     # TODO futures_contract.specific_log
-    # TODO data_blob.update_log
-    # TODO data.update_log(contract_object.specific_log(data.log))
-    # TODO data_blob._get_specific_logger
     # TODO log_with_attributes
 
     """
