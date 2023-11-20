e149efa40241940548e5361788a07f0b4fcab0b3
Rob Carver <robcarver17@gmail.com>
2023-11-20 14:20:51 +0000

    added position data to backup

--- sysdata/parquet/parquet_historic_strategy_positions.py
+++ sysdata/parquet/parquet_historic_strategy_positions.py
@@ -24,7 +24,6 @@ class parquetStrategyPositionData(strategyPositionData):
         return "parquetStrategyPositionData"
 
     @property
-    @property
     def parquet(self):
         return self._parquet
 
