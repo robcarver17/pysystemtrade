3c8304435c3db6de74c2cb634029061980c1b037
Rob Carver <robcarver17@gmail.com>
2023-11-22 09:31:22 +0000

    slippage reporting append error

--- sysproduction/reporting/data/trades.py
+++ sysproduction/reporting/data/trades.py
@@ -123,7 +123,7 @@ def raw_slippage_row(order_row):
     ) = price_calculations_for_order_row(order_row)
     new_order_row = copy(order_row)
     new_order_row = new_order_row[NEW_ORDER_ROW_COLS]
-    new_order_row = new_order_row.append(
+    new_order_row = new_order_row._append(
         pd.Series(
             [
                 delay,
