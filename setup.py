198023a8289875b83ab353353e22fecde67a76c4
Rob Carver <robcarver17@gmail.com>
2023-11-27 09:17:06 +0000

    arctic was still in setup and requirements

--- setup.py
+++ setup.py
@@ -91,7 +91,6 @@ setup(
         "numpy>=1.24.0",
         "scipy>=1.0.0",
         "pymongo==3.11.3",
-        "arctic==1.79.2",
         "psutil==5.6.6",
         "pytest>6.2",
         "Flask>=2.0.1",
