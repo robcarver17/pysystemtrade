babc99296ccf6fe437f369fd69fb49cf3c56b8ad
Andrew <acraig@gmail.com>
2024-05-03 10:27:28 -0400

    Subclass required for fx price data import

--- sysinit/futures/repocsv_spotfx_prices.py
+++ sysinit/futures/repocsv_spotfx_prices.py
@@ -4,9 +4,14 @@ Get FX prices from csv repo files and write to arctic
 WARNING WILL OVERWRITE EXISTING!
 """
 from sysdata.csv.csv_spot_fx import csvFxPricesData
-from sysproduction.data.currency_data import fxPricesData
+from sysdata.parquet.parquet_spotfx_prices import parquetFxPricesData
+from sysdata.data_blob import dataBlob  # Update with correct import path
 
-db_fx_price_data = fxPricesData()
+# Create a dataBlob instance which automatically uses the default parquet_store_path from the config
+blob = dataBlob()
+
+# Use the parquet_access from blob to create parquetFxPricesData
+db_fx_price_data = parquetFxPricesData(parquet_access=blob.parquet_access)
 
 if __name__ == "__main__":
     input("Will overwrite existing prices are you sure?! CTL-C to abort")
@@ -16,6 +21,7 @@ if __name__ == "__main__":
     currency_code = input("Currency code? <return for ALL currencies> ")
     if currency_code == "":
         list_of_ccy_codes = csv_fx_prices.get_list_of_fxcodes()
+        print(list_of_ccy_codes)
     else:
         list_of_ccy_codes = [currency_code]
 
