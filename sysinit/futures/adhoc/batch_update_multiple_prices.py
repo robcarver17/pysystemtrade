b06fb6116c8f0c55e2d2b53493b42c110c0c73c5
Victor Caldas <caldas.victor@gmail.com>
2024-10-20 23:00:05 +0200

    All products

--- sysinit/futures/adhoc/batch_update_multiple_prices.py
+++ sysinit/futures/adhoc/batch_update_multiple_prices.py
@@ -3,12 +3,11 @@ from sysproduction.data.prices import diagPrices
 from sysinit.futures.seed_price_data_from_IB import seed_price_data_from_IB
 diag_prices = diagPrices()
 
-MYLIST = ['BB3M', 'BRE', 'BRENT-LAST', 'BRENT_W', 'CHF_micro', 'CNH-onshore', 'COCOA', 'COCOA_LDN', 'COFFEE', 'DOW_mini', 'ETHANOL', 'ETHER-micro', 'ETHEREUM', 'EU-AUTO', 'EU-BANKS', 'EU-BASIC', 'EU-CHEM', 'EU-TECH', 'EU-UTILS', 'EUA', 'EURIBOR', 'EURIBOR-ICE', 'FED', 'GAS-LAST', 'GAS-PEN', 'GASOIL', 'GASOILINE', 'HEATOIL', 'LUMBER-new', 'MIB', 'MUMMY', 'MXP', 'NASDAQ', 'NASDAQ_micro', 'NICKEL_LME', 'NIKKEI', 'NIKKEI400', 'NOK', 'NZD', 'OAT', 'OATIES', 'OMX', 'PALLAD', 'PLAT', 'PLN', 'R1000', 'REDWHEAT', 'SONIA3', 'SOYBEAN', 'SOYBEAN_mini', 'SOYMEAL', 'SOYOIL', 'SP400', 'SP500', 'SP500_micro', 'SPI200', 'US-REALESTATE', 'US2', 'US3', 'US30', 'V2X', 'VIX_mini', 'VNKI']
 if __name__ == "__main__":
     errored = []
     csv_multiple_prices = csvFuturesMultiplePricesData()
     instrument_list = csv_multiple_prices.get_list_of_instruments()
-    for instrument_code in MYLIST:
+    for instrument_code in instrument_list:
         try:
             seed_price_data_from_IB(instrument_code)
         except Exception as e:
