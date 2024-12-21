e86e53cec63688069897807135364e10e23578af
anannnchim <nachimchim@gmail.com>
2024-12-22 06:21:06 +0700

    modified default.yaml file

--- my_change_log.md
+++ my_change_log.md
@@ -5,3 +5,7 @@
 1. Install `dnspython`
 - run `pip install dnspython` in (.venv)
 - add "dnspython" in `pyproject.toml`
+
+2. Update paht in `default.yaml`
+- add `parquet_store: '/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet'`
+
--- program/connect_mongo.py
+++ program/connect_mongo.py
@@ -6,8 +6,17 @@ if __name__ == '__main__':
 
     # 1. Get instance
     mongo_instance = mongoDb()
-    print(mongo_instance)
 
-    # 2. Get collection
+    # 2. Get db
+    db = mongo_instance.db
+
+    # 3. Get collection
+    collections = db.list_collection_names()
+
+    # 4. Print all out
+    for collection in collections:
+        print(collection)
+
+    # Note 2. Get specific collection
     # mongo_conn = mongoConnection("spread_costs") # rep as db
     # print(mongo_conn.collection)
